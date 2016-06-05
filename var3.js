e97c7b2f29fba52d9b5391bb749ff859b18399ec
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-05 13:42:05 +0600

    Create var3.js

---
+++ var3.js
@@ -0,0 +1,8 @@
+// Not sure where to begin? Check the Hint!
+var name = "codecademy";
+if (name.length > 0){
+    console.log("I finished my first course!");
+}
+else {
+    console.log("Error Error!");
+}
