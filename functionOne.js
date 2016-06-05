31fa4e5b87fdbb2127c80c6253ba16584c8b2a92
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-05 21:34:11 +0600

    Create functionOne.js

---
+++ functionOne.js
@@ -0,0 +1,9 @@
+// Write your foodDemand function below.
+// Last hint: In your reusable block of code, end each line
+// with a semicolon (;)
+// foodDemand is a function name
+//food is parameter name.
+var foodDemand = function(food){
+    console.log("I want to eat" + " " + food);
+}
+foodDemand("Hamburgers");
