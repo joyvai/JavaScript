1d1b8a7e9ae311c12265f4783b32024ecded124e
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-05 13:07:10 +0600

    Update substring.js

--- substring.js
+++ substring.js
@@ -2,5 +2,5 @@
 // Here is an example of the 1st to 4th letter of "JavaScript":
 // console.log("JavaScript".substring(0,4));
 console.log("January".substring(0,3));
-console.log("Melbourne is great".substring(0,10));
-console.log("Hamburgers".substring(4));
+console.log("Melbourne is great".substring(0,13));
+console.log("Hamburgers".substring(3));
