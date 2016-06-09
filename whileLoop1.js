e580bfa231451edd562f721f3a62861a4c9f107b
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-09 13:18:34 +0600

    Create whileLoop1.js

---
+++ whileLoop1.js
@@ -0,0 +1,11 @@
+//Remember to make your condition true outside the loop!
+var i = true;
+var soloLoop = function(){
+  //Your code goes here!
+  while (i){
+      console.log("Looped once!");
+      i=false;
+      }
+};
+
+soloLoop();
