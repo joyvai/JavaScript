00c124c52eef745125d119269a011764e3a70c50
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-09 13:11:20 +0600

    while loop

---
+++ whileLoop.js
@@ -0,0 +1,13 @@
+//Remember to set your condition outside the loop!
+var i =3;
+
+var loop = function(){
+	while(i){
+		//Your code goes here!
+		console.log("I'm looping!");
+		i--;
+	    
+	}
+};
+
+loop();
