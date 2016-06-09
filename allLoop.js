196d3625be6f217fcb1b6a3934bb465e1563e383
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-09 14:05:05 +0600

    Create allLoop.js

---
+++ allLoop.js
@@ -0,0 +1,24 @@
+// Write your code below!
+
+var i = true;
+var j = false;
+
+for (a=0;a<10;a++){
+    if ("myname".length === 6){
+        console.log("My name is joy");
+        }
+    else{
+        console.log("OOww!check that again!");
+        }
+    }
+while (i){
+    for (i=0;i<10;i++){
+        if (i%2){
+            console.log(i);
+            }
+        }
+    i=false;
+    
+    }
+do {console.log("This is a do while loop!");}
+while(j);
