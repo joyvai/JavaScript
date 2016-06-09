9b32147787d3add4402df2a70aac0de6fd6e7475
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-09 14:23:39 +0600

    Create myDragon.js

---
+++ myDragon.js
@@ -0,0 +1,13 @@
+// a variable to check if we're still slaying
+var slaying = true;
+// This youHit saves a random number which is 0 or 1.
+// 0=false and 1=true
+var youHit = Math.floor(Math.random()*2);
+// this damageThisRound saves a number 1 to 5 (including 5)
+var damageThisRound=Math.floor(Math.random()*5+1)
+var totalDamage = 0;
+
+while (slaying){
+    slaying = false;
+    
+    }
