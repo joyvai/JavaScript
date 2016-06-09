2068a29a906243681267cdcba09b918645d2aec4
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-09 13:35:26 +0600

    forwhile
    while loop inside the fooor loop

---
+++ forWhile.js
@@ -0,0 +1,12 @@
+var i = true;
+var a=3;
+var b = 4;
+for (j = 0 ; j < 5;j++){
+    if (b>a){
+        while (i){
+            console.log("I am a while loop");
+            i=false;
+            }
+        }
+    
+    }
