653afa118d96ed93742124501de48c5b141d0130
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-09 13:55:04 +0600

    Create dowhile10.js

---
+++ dowhile10.js
@@ -0,0 +1,10 @@
+var getToDaChoppa = function(){
+  // Write your do/while loop here!
+    var loopCondition=false;
+    do{
+     console.log("I am do While loop!");
+    }
+    while(loopCondition);
+};
+
+getToDaChoppa();
