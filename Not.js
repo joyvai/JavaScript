8c3b610981cf820bea7df175750fba31c36f4667
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-11 20:22:53 +0600

    Create Not.js

---
+++ Not.js
@@ -0,0 +1,20 @@
+
+// The logical operator not is written in JavaScript like this: 
+// !. It makes true expressions false, and vice-versa.
+//
+// !true;   // => false
+// !false;  // => true
+
+// Declare your variables here!
+var programming = false;
+
+
+var happy = function() {
+  // Add your if/else statement here!
+  if (!programming){
+    return true;
+    }
+    else{
+        return false;
+    }
+};
