646a65eb46a9ebeb5cc201c4eb5259681cb13230
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-11 19:29:18 +0600

    Create switch1.js

---
+++ switch1.js
@@ -0,0 +1,14 @@
+var answer = prompt("Add your question here!");
+
+switch(answer) {
+  case 'what is your name':
+    console.log("My name is joy.");
+    break;
+  // Add your code here!
+  case 'What is your gf name':
+      console.log("I don't have gf.");
+      break;
+default:
+    console.log("okay fucker.Go to Hell!");
+    break;
+}
