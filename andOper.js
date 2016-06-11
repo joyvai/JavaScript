e23478174db7b2f4e745d42c72016c59e5635689
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-11 20:13:46 +0600

    Create andOper.js

---
+++ andOper.js
@@ -0,0 +1,14 @@
+// Declare your variables here!
+var hungry = true;
+var foodHere = true;
+
+
+var eat = function() {
+  // Add your if/else statement here!
+  if (hungry && foodHere){
+      return true;
+    }
+    else{
+        return false;
+    }   
+};
