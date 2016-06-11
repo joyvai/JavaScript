5cd45aa974b08129520f36d6e2ffa9f65aa7ed7f
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-11 21:59:39 +0600

    Create object1.js

---
+++ object1.js
@@ -0,0 +1,10 @@
+var object1=new Object();
+var object2=new Object();
+var object3=new Object();
+
+object1["name"]="joy";
+object1.age=21;
+object2["skills"]=['html','css','js'];
+object2.job="jobless";
+object3["hobby"]="reading books";
+object3.flim='hobbit';
