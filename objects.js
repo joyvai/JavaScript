5a150fd5520f1b5d26e670ab1e4e255fe39462ac
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-11 21:38:38 +0600

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,12 @@
+/*
+Using objects, we can put our information and the functions that use that information in the same place.
+
+You can also think of objects as combinations of key-value pairs (like arrays), 
+only their keys don't have to be numbers like 0, 1, or 2: they can be strings and variables.
+
+*/
+var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
+
+for (var i=0;i<languages.length;i++){
+    console.log(languages[i]);    
+}
