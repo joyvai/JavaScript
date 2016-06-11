ac7a78b45c892232443ff577aa011d01c3981fb3
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-11 20:05:19 +0600

    Create OwnSwitch.js

---
+++ OwnSwitch.js
@@ -0,0 +1,24 @@
+// Write your code below!
+// Write your code below!
+
+var a =prompt("Hi,sir.Which country you want to travel?");
+
+switch(a){
+    case 'USA':
+        console.log("Usa is not right place for muslims.");
+        break;
+    case 'uk':
+        console.log("uk is best place for tourist.");
+        break;    
+    case 'canada':
+        console.log("Canada is great");
+        break;
+    case 'Bangladesh':
+        console.log("Bangladesh is a peaceful country.");
+        break;
+    case 'Dubai':
+        console.log("Yeah!!That's good.");
+        break;
+default:
+    console.log("Ok.You don't travel.");
+    }
