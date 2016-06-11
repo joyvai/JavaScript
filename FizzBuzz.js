46a03c160bfe74556e104b0af74bfd200f132eb0
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-12 00:38:04 +0600

    Create FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,13 @@
+for(i=1;i <= 20; i++){
+if(i % 3 === 0 || i % 5 === 0){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");            
+    }else if(i % 3 === 0){
+        console.log("Fizz");
+    }else{
+        console.log("Buzz");        
+    }
+}else{
+    console.log(i);        
+}
+}
