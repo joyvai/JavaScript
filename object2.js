9165a341692d49213b48aec07cadfa4669039dcb
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-12 16:14:54 +0600

    Create object2.js

---
+++ object2.js
@@ -0,0 +1,17 @@
+// Take a look at our next example object, a dog
+var dog = {
+  species: "greyhound",
+  weight: 60,
+  age: 4
+};
+
+var species = dog["species"];
+// Here dog is a object name and "species" is a dog object //property.
+// fill in the code to save the weight and age using bracket notation
+var weight = dog ["weight"];
+var age =dog["age"];
+
+console.log(dog);
+console.log(species);
+console.log(weight);
+console.log(age);
