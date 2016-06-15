1d10c9e9e29019e54eeb495768a2cb8d682cfb0f
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-15 21:22:14 +0600

    Create LiteralVsCons.js

---
+++ LiteralVsCons.js
@@ -0,0 +1,22 @@
+//What Are Objects For?
+/*
+Objects provide us with a way to represent real-world
+or virtual things. We can do this by storing information
+inside the object's properties.There are two basic ways to make objects:
+*/
+//Literal Notation, where we use=> var Name = { };
+//Constructor Notation, where we use the keyword new.
+
+
+//Literal Notation{}
+var spencer = {
+  age: 22,
+  country: "United States"
+};
+
+// make spencer2 here with constructor notation
+// Constructor Notation
+
+var spencer2=new Object();
+spencer2.age = 22;
+spencer2.country='United States';
