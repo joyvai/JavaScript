241eeab5bb7727acf9320a73fdc6dd9ed21fc266
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-17 23:54:03 +0600

    Rename switch.js to switchLoop.js

---
+++ switchLoop.js
@@ -0,0 +1,23 @@
+var user = prompt("What is your Name?").toUpperCase();
+
+switch(prompt("How old are you?")) {
+    case '20':
+        console.log("You are 20");
+        break
+    case '21':
+        console.log("You are 21");
+        break;
+    case '22':
+        console.log("You are 22");
+        break;
+    case '23':
+        console.log('joy you are 23!!Really fuck you man!!');
+        break;
+    default:
+        console.log('fuck you!');
+        break;
+    
+    
+
+
+}
