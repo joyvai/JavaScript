ded7f6d87cc0be62f34b8f087402ffcbc3046b31
iftekharul islam <iftekharjoy1@gmail.com>
2016-06-18 22:08:10 +0600

    Create seven.js

---
+++ OBJECTS/seven.js
@@ -0,0 +1,10 @@
+var myObj = {
+    // finish myObj
+    name:'joy',
+    nicname:''
+    
+    
+};
+
+console.log( myObj.hasOwnProperty('name') ); // should print true
+console.log( myObj.hasOwnProperty('nickname') ); // should print false
