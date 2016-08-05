8e6bf103e06eb84591992cc614ad1b122d9839a8
iftekharul islam <iftekharjoy1@gmail.com>
2016-08-05 23:42:00 +0600

    Arrary Constructor

---
+++ Personal-Code/arrayConstructor.js
@@ -0,0 +1,4 @@
+var colors = new Array ('White','black','custom');
+// This is an array constructor.Here new keyword follewed by Array().
+var el = document.getElementById('colors');
+el.innerHTML= colors.item(0);
