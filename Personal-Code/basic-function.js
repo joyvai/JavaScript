7862813b0d4ef08e034b615949e0e01d3642ed8a
iftekharul islam <iftekharjoy1@gmail.com>
2016-08-06 22:05:52 +0600

    basic js

---
+++ Personal-Code/basic-function.js
@@ -0,0 +1,21 @@
+var msg = 'Sign up to receive our newsletter for 10% off!';
+function updateMessage(){
+	var el = document.getElementById('message');
+	el.textContent = msg;
+}
+updateMessage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
