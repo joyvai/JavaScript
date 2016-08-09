20abe3c7c8444c4ca376c049437c44f47b90e58e
iftekharul islam <iftekharjoy1@gmail.com>
2016-08-09 13:16:06 +0600

    Create number-object.js

---
+++ Personal-Code/number-object.js
@@ -0,0 +1,6 @@
+var originalNumber = 10.23456;
+var msg ='<h2>original Number</h2><p>' + originalNumber +'</p>';
+msg += '<h2>toFixed()</h2><p>'+originalNumber.toFixed(3);+'</p>';
+msg += '<h2>toPrecision()</h2><p>' +originalNumber.toPrecision(3)+'</p>';// 3digit
+var el = document.getElementById('info');
+el.innerHTML = msg;
