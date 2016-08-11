b9d74e07afe8a1d0b78f6003586f6f1cce77868a
iftekharul islam <iftekharjoy1@gmail.com>
2016-08-12 00:47:24 +0600

    Create first-child.js

---
+++ Personal-Code/first-child.js
@@ -0,0 +1,18 @@
+// select the starting point and find its children
+var startItem = document.getElementsByTagName('ul')[0];
+var firstItem = startItem.firstChild;
+var lastItem = startItem.lastChild;
+
+// Change the values of the children's class attributes
+firstItem.setAttribute('class','complete');
+lastItem.setAttribute('class','cool');
+
+
+
+
+
+
+
+
+
+
