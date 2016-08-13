0c1d4c4cc712815957abef4a3df2e1cd3c00b238
iftekharul islam <iftekharjoy1@gmail.com>
2016-08-14 00:07:14 +0600

    Create remove-element.js

---
+++ Personal-Code/remove-element.js
@@ -0,0 +1,3 @@
+var removeEl = document.getElementsByTagName('li')[3]; // The element to remove
+var containerEl = removeEl.parentNode; // Its containing element
+containerEl.removeChild(removeEl); // Removing the element
