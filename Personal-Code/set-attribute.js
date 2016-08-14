b82bbe90a19727cd2524921bc4827f7756d3aa97
iftekharul islam <iftekharjoy1@gmail.com>
2016-08-15 00:22:48 +0600

    Create set-attribute.js

---
+++ Personal-Code/set-attribute.js
@@ -0,0 +1,17 @@
+var firstItem = document.getElementById('one'); // get the first firstItem
+firstItem.className='complete';
+//get the fourth item
+var fourthItem = document.getElementsByTagName('li').item(3);
+fourthItem.setAttribute('class','cool');
+
+// className property change the value class attribute.
+
+var secondItem = document.getElementById('two');
+secondItem.className = 'pine';
+
+var thirdItem = document.getElementsByTagName('li').item(2);
+thirdItem.setAttribute('class','honey');
+
+// This code we change the class name value using setAttribute() and className
+// You can use any technique.
+
